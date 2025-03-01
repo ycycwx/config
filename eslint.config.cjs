b879173a6f245dc56932289dcc9ba909a88b5acc
ycycwx <yytcjcy@gmail.com>
2025-03-01 19:23:18 +0800

    style: change to eslint.config.mjs

--- eslint.config.cjs
+++
@@ -1,6 +0,0 @@
-const {flatConfigs} = require('./eslint');
-
-/**
- * @type {import('typescript-eslint').ConfigArray}
- */
-module.exports = flatConfigs.config('node');
---
+++ eslint.config.mjs
@@ -0,0 +1,4 @@
+// eslint-disable-next-line import/extensions -- fix it after refactor to esm
+import config from './eslint/flat/config.js';
+
+export default config('node');
