0d63bd13c0b01d06903b812d40904d4a1523a6b9
ycycwx <yytcjcy@gmail.com>
2025-03-02 18:23:39 +0800

    feat!: support esm usage of eslint flat config and drop commonjs (#528)

--- eslint/index.js
+++ eslint/index.js
@@ -1,7 +1,31 @@
-const build = require('./flat/build');
-const config = require('./flat/config');
+import base from './base.js';
+import browser from './browser.js';
+import node from './node.js';
+import react from './react.js';
+import typescript from './typescript.js';
 
-module.exports.flatConfigs = {
-    build,
-    config,
+/**
+ * @typedef {'node' | 'browser' | 'react' | 'typescript'} Preset
+ */
+
+/**
+ * Create eslint flat config
+ *
+ * @param {Array<Preset> | Preset | undefined} presets
+ * @param {import('typescript-eslint').ConfigArray} extendedFlatConfigs
+ * @returns {import('typescript-eslint').ConfigArray}
+ */
+export default (presets, ...extendedFlatConfigs) => {
+    /**
+     * @type {Preset[]}
+     */
+    const resolvedPresets = Array.isArray(presets ?? []) ? presets : [presets];
+    return [
+        ...base,
+        ...resolvedPresets.includes('node') ? node : [],
+        ...resolvedPresets.includes('browser') ? browser : [],
+        ...resolvedPresets.includes('react') ? react : [],
+        ...resolvedPresets.includes('typescript') ? typescript : [],
+        ...extendedFlatConfigs,
+    ];
 };
